25ec66be4f2c33ef375ca175b01f34abed3203ba
bharathkumar-rajagopal <i.bharath@outlook.com>
2025-03-24 13:14:27 +0000

    Deploying to gh-pages from @ bharathkumar-rajagopal/bharathkumar-rajagopal.github.io@40ef08c3f58a7258f230a66e21070184e1c604f1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "A growing collection of my cool projects.",
+          description: "A growing collection of some wonderful projects I have been a part of.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
