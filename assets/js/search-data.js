12213076f1bef0f7e8e2f9ebc55ecdfdaf09cc3f
bharathkumar-rajagopal <i.bharath@outlook.com>
2025-08-22 06:01:43 +0000

    Deploying to gh-pages from @ bharathkumar-rajagopal/bharathkumar-rajagopal.github.io@21a2e0710fd4682833cc2b57a92de68919832fe2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-publications",
           title: "Publications",
-          description: "My publications in reverse chronological order (§ contributed equally).",
+          description: "My publications in reverse chronological order (§Equal contribution).",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
