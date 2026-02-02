4f0708495b41a8d850dea5abe0490575602e9fb6
bharathkumar-rajagopal <i.bharath@outlook.com>
2026-02-02 16:12:36 +0000

    update: remove author_html field from BibTeX output and clean author lists

--- _plugins/hide-custom-bibtex.rb
+++ _plugins/hide-custom-bibtex.rb
@@ -7,8 +7,8 @@ module Jekyll
 		    input = input.gsub(/^.*\b#{keyword}\b *= *\{.*$\n/, '')
 	    end
 
-      # Clean superscripts in author lists
-      input = input.gsub(/^.*\bauthor\b *= *\{.*$\n/) { |line| line.gsub(/[*†‡§¶‖&^]/, '') }
+      # Remove author_html field (custom field, not needed in BibTeX output)
+      input = input.gsub(/^.*\bauthor_html\b *= *\{.*$\n/, '')
 
       return input
     end
